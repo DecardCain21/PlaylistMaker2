d03e6694e88e7473902582c7da75cc6e52039b86
Cain <decardcain21@gmail.com>
2024-06-18 17:54:46 +0300

    HostActivity created

---
+++ app/src/main/java/com/marat/hvatit/playlistmaker2/presentation/HostActivity.kt
@@ -0,0 +1,7 @@
+package com.marat.hvatit.playlistmaker2.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import com.marat.hvatit.playlistmaker2.R
+
+class HostActivity : AppCompatActivity(R.layout.activity_host) {
+}
